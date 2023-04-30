50649bac97393076ea3f2564477a8a6915561723
Derek Chan <derekchancts@yahoo.com.hk>
2023-04-30 10:42:36 +0800

    configure github actions

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const path = require('path')
 
 app.use(cors())
+// app.use(cors({ origin: 'https://localhost' }))
 
-app.get('/names', (req, res) => res.send('Derek Derek'))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+
+app.get('/names', (req, res) => {
+  res.send('Derek Derek')
+})
 
 app.listen(5000, () => console.log('Server is running on port 5000'))
